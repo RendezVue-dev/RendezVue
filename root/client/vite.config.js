1c78357d9792171a61b953804d5b6cd60909362a
FayNguyen03 <ntkhanh391914@gmail.com>
2025-11-20 02:25:21 -0600

    Fix the vite config to deploy

--- root/client/vite.config.js
+++ root/client/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, './.env') });
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.API_URL,
         changeOrigin: true,
         secure: false,
       },
